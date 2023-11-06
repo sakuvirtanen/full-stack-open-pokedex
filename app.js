4886b47faf3b0d4638cdab88ae41acc2d99be00e
Saku Virtanen <saku@virtanen.co>
2023-11-06 19:14:53 +0200

    Exercise 11.11. Added default tcp_check back as I had deleted it previously. Fly's http_check and supporting endpoint /health created

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('Exercise 11.11 v0') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
