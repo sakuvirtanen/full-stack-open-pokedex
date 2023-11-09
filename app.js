6b9fd8ee27135292424d4547071de4da79f0b696
Saku Virtanen <saku@virtanen.co>
2023-11-09 20:43:54 +0200

    Exercise 11.11. Put the error in the wrong endpoint. THIS should fail the pipeline, next commit will fix.

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
+  throw new Error('This should fail my pipeline')
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  throw new Error('This should fail my pipeline')
-  // eslint-disable-next-line no-unreachable
   res.send('Exercise 11.11 v1')
 })
 
